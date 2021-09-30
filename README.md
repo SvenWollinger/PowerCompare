a06a015a512418df7eb45b455d0fe3bf82b0d719
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-09-30 23:58:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PowerCompare
+ Compare tool
---
+++ src/io/wollinger/powercompare/PowerCompare.java
@@ -0,0 +1,10 @@
+package io.wollinger.powercompare;
+
+public class PowerCompare {
+    public static void main(String[] args) {
+        if(args.length <= 2)
+            System.out.println("Please provide atleast two paths!");
+
+
+    }
+}
